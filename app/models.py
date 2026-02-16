ccf40ff5336d038c61cbbaac4447210b80a64fde
adam-benyekkou <s.benyekkou@gmail.com>
2026-02-16 13:18:49 +0100

    fix(quality): remove unused import and add verification results

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@
 
 from datetime import datetime
 from typing import Optional, List
-from sqlmodel import SQLModel, Field, JSON, Column
+from sqlmodel import SQLModel, Field, Column
 from sqlalchemy import JSON as SAJSON
 
 
